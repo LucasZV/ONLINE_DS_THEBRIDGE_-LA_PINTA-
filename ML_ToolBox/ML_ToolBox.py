7926870a4d0674f4052f9d44e60d4411de02fe0d
enrikrecido <ramcoben95@gmail.com>
2024-02-09 19:02:12 +0100

    ARREGLITOS
    YES

--- ML_ToolBox/ML_ToolBox.py
+++ ML_ToolBox/ML_ToolBox.py
@@ -108,7 +108,8 @@ def tipifica_variables(df, umbral_cat: int, umbral_con: float):
 
 # Get Features Num Regression
 def get_features_num_reggresion(df,target_col, umbral_corr,pvalue=None):
-    """
+    if target_col not in df.columns:
+        """
     get_features_num_regresion: selecciona las características numéricas para un problema de regrersión. 
     Esta función verifica que los datos sean adecuados y automáticamente selecciona las columnas numéricas que
     están más relacionadas con la que estamos tratando. Sirve para hacer predicciones más precisas.
@@ -122,21 +123,19 @@ def get_features_num_reggresion(df,target_col, umbral_corr,pvalue=None):
     Retorna:
     - selected_features (list): Lista de características seleccionadas que cumplen con los criterios.
     """  
-    if target_col not in df.columns:
-        print(f"Error: La columna '{target_col}' no esta en el dataframe")
-
+    
     if not np.issubdtype(df[target_col].dtype, np.number):# Comprobar que la columna objetivo es una variable numérica continua
         print(f"Error: La columna '{target_col}' no es una variable numérica continua.")
         return None
-        
+    
     if not (0 <= umbral_corr <= 1): # Comprobar que el umbral de correlación está entre 0 y 1
         print("Error: umbral_corr debe estar entre 0 y 1")
         return None 
-        
+    
     if pvalue is not None and not (0 <= pvalue <= 1):# Comprobar que pvalue es None o un número entre 0 y 1
         print("Error: pvalue debe ser None o un número entre 0 y 1.")
         return None
-        
+    
     numeric_cols = df.select_dtypes(include=np.number).columns.tolist()  # Obtener columnas numéricas del DataFrame
 
     Correlations = []  # Calcular la correlación y p-values para cada columna numérica con 'target_col'
@@ -149,69 +148,65 @@ def get_features_num_reggresion(df,target_col, umbral_corr,pvalue=None):
     for col, corr, p_value in Correlations:
         if abs(corr) > umbral_corr and (pvalue is None or p_value <= (1 - pvalue)):
             selected_features.append(col)
+
+    if not selected_features: #Si no tiene correlación nos imprime este mensaje:
+                 return "No hay correlación"        
+
     return selected_features
 
+#ejemplo de uso
+# get_features_num_reggresion(df, 'target_column', 0.5, pvalue=0.05)
+
+
+
 # Plot Features Num Regression
-def plot_features_num_regression(dataframe, target_col="", columns=[], umbral_corr=0, pvalue=None):
+def plot_features_num_regression(dataframe, target_col="", columns=[]):
     """
     Genera un pairplot para características numéricas basado en la correlación con la columna objetivo.
 
     Parámetros:
     - dataframe (pd.DataFrame): DataFrame de entrada.
-    - target_col (str, opcional): Columna objetivo para el análisis de correlación.
-    - columns (lista de str, opcional): Lista de columnas para el pairplot. Por defecto, es una lista vacía.
-    - umbral_corr (float, opcional): Umbral de correlación. Por defecto, es 0.
-    - pvalue (float o None, opcional): Valor p para el test de significación estadística de la correlación. Por defecto, es None.
+    - target_col (str): Columna objetivo para el análisis de correlación.
+    - columns (lista de str): Lista de columnas para el pairplot.
 
     Retorna:
-    - lista de str: Lista de columnas que cumplen con las condiciones específicas.
+    - None
     """
-
-    # Valoresde entrada
+    # Verifica que el argumento 'dataframe' sea un DataFrame de pandas.
     if not isinstance(dataframe, pd.DataFrame):
         raise ValueError("El argumento 'dataframe' debe ser un DataFrame de pandas.")
 
+    # Verifica que 'target_col' esté en las columnas del DataFrame.
     if target_col not in dataframe.columns:
         raise ValueError("La columna objetivo '{}' no se encuentra en el dataframe.".format(target_col))
 
+    # Verifica que 'columns' sea una lista de strings.
     if not isinstance(columns, list):
         raise ValueError("'columns' debe ser una lista de strings.")
 
+    # Verifica que todas las columnas en 'columns' existan en el DataFrame.
     if not all(col in dataframe.columns for col in columns):
         raise ValueError("Todas las columnas en 'columns' deben existir en el dataframe.")
 
-    if not isinstance(umbral_corr, (int, float)):
-        raise ValueError("'umbral_corr' debe ser un valor numérico.")
-
-    if not (isinstance(pvalue, (float, int)) or pvalue is None):
-        raise ValueError("'pvalue' debe ser un valor numérico o None.")
-
-    if not columns: # Si 'columns' está vacío, utiliza todas las columnas numéricas
+    # Si 'columns' está vacío, utiliza todas las columnas numéricas.
+    if not columns:
         columns = dataframe.select_dtypes(include=['number']).columns.tolist()
 
-    selected_columns = []  # Filtra las columnas basadas en la correlación y el valor p
+    # Imprime la correlación y p-values entre cada columna numérica y 'target_col'.
     for col in columns:
         if col != target_col:
             correlation, p_val = pearsonr(dataframe[col], dataframe[target_col])
+            print(f"Correlación entre {col} y {target_col}: {correlation:.4f}, p-value: {p_val:.4f}")
 
-            if abs(correlation) > umbral_corr and (pvalue is None or p_val < pvalue):
-                selected_columns.append(col)
+    # Genera un pairplot para las columnas seleccionadas.
+    subset_columns = [target_col] + columns
+    subset_data = dataframe[subset_columns]
+    sns.pairplot(subset_data, diag_kind='kde')
+    plt.show()
 
+# Ejemplo de uso
+# plot_features_num_regression(df, target_col='', columns=[''])
 
-
-    #  pairplot para las columnas seleccionadas
-    if selected_columns:
-        num_plots = len(selected_columns) // 4 + 1
-        for i in range(num_plots):
-            start_index = i * 4
-            end_index = (i + 1) * 4
-            subset_columns = [target_col] + selected_columns[start_index:end_index]
-            
-            subset_data = dataframe[subset_columns]
-            sns.pairplot(subset_data, diag_kind='kde')
-            plt.show()
-
-    return selected_columns
 
 # Get Features Cat Regression
 def get_features_cat_regression(df,target_col,pvalue=0.05,umbral_card=0.5):
